c39586243d354eef16b2f38d3d31aec81ddfa1e1
mikey <mike.owino40@yahoo.com>
2022-12-01 12:10:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+
+# window = tk.Tk()
+# window.title("Hello") # Sets the name of the window in the border
+# window.geometry("300x250") # Sets the size of the window in pixels
+
+# label = "Hey there"
+# hello = tk.Label(text = label)
+# # hello = tk.Label(text = "Hello World") # Creates a text box
+# hello.pack() # 'pack' places the element on the screen
+
+# button = tk.Button(text = "Click me!") # Creates a button
+# button.pack()
+
+# tk.mainloop()
+
+
+# window = tk.Tk()
+# window.title("Hello World") 
+# window.geometry("300x450") 
+
+# label = "Hey there world"
+
+# def updateLabel():
+#   label = "Bye world!"
+#   hello["text"] = label 
+#   # Subroutine that updates the text in the label.
+
+# hello = tk.Label(text = label) 
+# hello.pack() 
+
+# button = tk.Button(text = "Click me!", command = updateLabel) # Calls the updateLabel subroutine when the button is clicked
+# button.pack()
+
+# tk.mainloop()
+
+
+# window = tk.Tk()
+# window.title("Hello World") 
+# window.geometry("300x300") 
+
+
+# label = 0 # Sets the starting label value to 0
+
+# def updateLabel():
+#   global label # Uses the values in the label variable
+#   label += 1 # Adds one to the value in the label
+#   hello["text"] = label 
+  
+
+# hello = tk.Label(text = label) 
+# hello.pack() 
+
+# button = tk.Button(text = "Click me!", command = updateLabel) # Calls the updateLabel subroutine when the button is clicked
+# button.pack()
+
+# tk.mainloop()
+
+# window = tk.Tk()
+# window.title("Hello World") 
+# window.geometry("300x300") 
+
+# label = 0
+
+# def updateLabel():
+#   global label
+#   label += 1 
+#   hello["text"] = label 
+  
+
+# hello = tk.Label(text = label) 
+# hello.pack() 
+
+# button = tk.Button(text = "Click me!", command = updateLabel) 
+# button.pack()
+
+# text = tk.Text(window ,height=1, width = 50)
+# text.pack
+
+# tk.mainloop()
+
+
